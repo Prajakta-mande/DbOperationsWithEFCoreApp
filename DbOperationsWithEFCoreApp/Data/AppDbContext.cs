5a91dd2e288fa0095dcff2298fdb26b319a5804f
Prajakta-mande <ppmande29@gmail.com>
2024-07-25 13:25:46 +0530

    installdbcontext class

---
+++ DbOperationsWithEFCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationsWithEFCoreApp.Data
+{//AppDbContext inherite this class from the class DbContext to get properties like options
+    public class AppDbContext:DbContext
+    {//AppDbContext constructor
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
